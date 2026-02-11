5766a306778930d54a8c831c1d312662dfa8b3ea
Repository Gang <1akieckhefer@gmail.com>
2026-02-11 02:38:39 -0800

    Update analytics.rs

--- src-tauri/src/analytics.rs
+++ src-tauri/src/analytics.rs
@@ -1,1 +1,13 @@
+use chrono::{Duration, Utc};
+
+pub fn last_14_days() -> Vec<String> {
+    let mut days = vec![];
+
+    for i in 0..14 {
+        let day = Utc::now() - Duration::days(i);
+        days.push(day.format("%Y-%m-%d").to_string());
+    }
+
+    days
+}
 
